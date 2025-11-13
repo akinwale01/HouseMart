eda0c08d5e79f703b1ce3f838cba4d2ec39f97ad
akinwale01 <akinwaleisrael2002@gmail.com>
2025-11-13 16:58:12 +0100

    fixd env vars for email in support message route and changed logout redirect

--- src/app/api/send-support-message/route.ts
+++ src/app/api/send-support-message/route.ts
@@ -13,8 +13,8 @@ export async function POST(req: Request) {
     const transporter = nodemailer.createTransport({
       service: "gmail", // you can also use 'smtp.office365', etc.
         auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.EMAIL_USER2,
+        pass: process.env.EMAIL_PASS2,
         },
     });
 
